01ecc9d8ef9ff0e63aa409533c617c2666812622
beltegeuse <adrien.gruson@gmail.com>
2025-04-10 00:57:08 -0400

    update simple

--- examples/simple.py
+++ examples/simple.py
@@ -3,14 +3,25 @@ import frnn
 import torch.utils.benchmark as benchmark
 
 # Sample 2 long array of 3D points
-a = torch.randn((1, 40000, 3), device="cuda")
-b = torch.randn((1, 100000, 3), device="cuda")
+a = torch.randn((1, 1000000, 3), device="cuda")
+b = torch.randn((1, 400000, 3), device="cuda")
 
+# idxs, counts, _, _ = frnn.frnn_grid_resampling(a, b, K=50, r=0.1)
 idxs, counts, _ = frnn.frnn_bf_resampling(a, b, K=50, r=0.1)
 counts = counts.view(-1)
 counts = counts.cpu().numpy()
 print("Counts shape:", counts.shape)
 print("Counts:", counts)
+
+# Count how many exceed 50
+exceed_50 = (counts > 50).sum()
+print("Number of counts exceeding 50:", exceed_50.item())
+# Count how many are less than 50
+less_than_50 = (counts < 50).sum()
+print("Number of counts less than 50:", less_than_50.item())
+
+pts = frnn.frnn_gather(b, idxs)
+print(pts)
 
 # Matplotlib to visualize the count distribution as a histogram
 import matplotlib.pyplot as plt
